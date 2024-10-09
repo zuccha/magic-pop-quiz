7f28e18e80633dca79ef576a92f1a46a8620e5bc
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2024-10-09 16:41:04 +0200

    feat: add header

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,26 @@
 import HomePage from "./pages/home-page";
+import QuizPage from "./pages/quiz-page";
 import "./app.css";
 
+const pages: Record<string, () => JSX.Element> = {
+  "/cards-quiz": QuizPage,
+};
+
 export default function App() {
+  const Page = pages[document.location.pathname] ?? HomePage;
+
   return (
     <div className="App">
-      <HomePage />
+      <div className="App_Header">
+        <div>
+          <a className="App_Header_Title" href="/">
+            <b>Home</b>
+          </a>
+        </div>
+      </div>
+      <div className="App_Page">
+        <Page />
+      </div>
     </div>
   );
 }
